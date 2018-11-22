7506b1937bfeca500ee877e2cfb2fb8ec827fb10
aurelieglassey <aurelie.glassey@students.hevs.ch>
2018-11-22 09:25:21 +0100

    Remove mainactivity and define Hitchhiker activity as main activity

---
+++ app/src/main/java/ch/safedrive/safedrive/ui/hitchhiker/request/HitchhikerActivity.java
@@ -0,0 +1,14 @@
+package ch.safedrive.safedrive.ui.hitchhiker.request;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import ch.safedrive.safedrive.R;
+
+public class HitchhikerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hitchhiker);
+    }
+}
