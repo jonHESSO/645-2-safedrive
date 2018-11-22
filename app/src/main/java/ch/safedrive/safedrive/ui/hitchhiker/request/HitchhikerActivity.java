334e6cca9d56c1a960085c729d5e61852486b195
Guordek <30309206+Guordek@users.noreply.github.com>
2018-11-22 10:06:21 +0100

    Implements CreateRequest fragment in HitchhikerActivity

--- app/src/main/java/ch/safedrive/safedrive/ui/hitchhiker/request/HitchhikerActivity.java
+++ app/src/main/java/ch/safedrive/safedrive/ui/hitchhiker/request/HitchhikerActivity.java
@@ -1,14 +1,38 @@
 package ch.safedrive.safedrive.ui.hitchhiker.request;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import ch.safedrive.safedrive.R;
 
-public class HitchhikerActivity extends AppCompatActivity {
+public class HitchhikerActivity extends AppCompatActivity implements CreateRequest.OnFragmentInteractionListener, Taking_picture.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hitchhiker);
+
+        Fragment fragment = null;
+        Class fragmentClass = null;
+
+        fragmentClass = CreateRequest.class;
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
